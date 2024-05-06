7b290de611174470fc2a8e6ef41d93d31504ac5f
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2024-05-06 13:50:17 +0200

    onlyoffice-package-builder.sh: Check parametres.

--- onlyoffice-package-builder.sh
+++ onlyoffice-package-builder.sh
@@ -19,3 +19,77 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################
 
+usage() {
+cat <<EOF
+
+  $0
+  Copyright BTACTIC, SCCL
+  Licensed under the GNU PUBLIC LICENSE 3.0
+
+  Usage: $0 --production-version=PRODUCT_VERSION --build-number=BUILD_NUMBER --unlimited-organization=ORGANIZATION --tag-suffix=-TAG_SUFFIX
+  Example: $0 --production-version=7.4.1 --build-number=36 --unlimited-organization=btactic-oo --tag-suffix=-btactic
+
+EOF
+
+}
+
+
+# Check the arguments.
+for option in "$@"; do
+  case "$option" in
+    -h | --help)
+      usage
+      exit 0
+    ;;
+    --production-version=*)
+      PRODUCTION_VERSION=`echo "$option" | sed 's/--production-version=//'`
+    ;;
+    --build-number=*)
+      BUILD_NUMBER=`echo "$option" | sed 's/--build-number=//'`
+    ;;
+    --unlimited-organization=*)
+      UNLIMITED_ORGANIZATION=`echo "$option" | sed 's/--unlimited-organization=//'`
+    ;;
+    --tag-suffix=*)
+      TAG_SUFFIX=`echo "$option" | sed 's/--tag-suffix=//'`
+    ;;
+  esac
+done
+
+
+if [ "x${PRODUCTION_VERSION}" == "x" ] ; then
+    cat << EOF
+    --production-version option must be informed.
+    Aborting...
+EOF
+    usage
+    exit 1
+fi
+
+if [ "x${BUILD_NUMBER}" == "x" ] ; then
+    cat << EOF
+    --build-number option must be informed.
+    Aborting...
+EOF
+    usage
+    exit 1
+fi
+
+if [ "x${UNLIMITED_ORGANIZATION}" == "x" ] ; then
+    cat << EOF
+    --unlimited-organization option must be informed.
+    Aborting...
+EOF
+    usage
+    exit 1
+fi
+
+if [ "x${TAG_SUFFIX}" == "x" ] ; then
+    cat << EOF
+    --tag-suffix option must be informed.
+    Aborting...
+EOF
+    usage
+    exit 1
+fi
+
